7551c9729252c8d8fdf5cc1361529f40f8738253
DDDrop <wylroro.dev@gmail.com>
2016-10-06 16:36:33 +0900

    Initial Commit

---
+++ MixPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MixPlayer
+//
+//  Created by go.yaro on 10/6/16.
+//  Copyright © 2016 DDDrop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
